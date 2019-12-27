f1fdb7fb68740f888bcf7d8439b04036a6bf2313
Guafindel <50958613+Guafindel@users.noreply.github.com>
2019-12-27 15:30:46 +0900

    15:30

--- SharedExample/app/src/main/java/com/example/sharedexample/MainActivity.java
+++ SharedExample/app/src/main/java/com/example/sharedexample/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.sharedexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edit_save;
+    private String shared = "file";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edit_save = (EditText)findViewById(R.id.edit_save);
+
+        SharedPreferences sharedPreferences = getSharedPreferences(shared, 0);
     }
 }
